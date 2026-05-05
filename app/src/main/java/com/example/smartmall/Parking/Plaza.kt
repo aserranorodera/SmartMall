d35a074476a0d9e212e6f893a4bafe92adfeec4a
AnaSerranoRodera <rodera.carrera18@gmail.com>
2026-05-05 17:37:12 +0200

    .

--- app/src/main/java/com/example/smartmall/Parking/Plaza.kt
+++ app/src/main/java/com/example/smartmall/Parking/Plaza.kt
@@ -4,12 +4,3 @@ data class Plaza(
     val id: Int,
     val libre: Boolean
 )
-
-/*
-*
-* Terminar parte de restaurantes
-* que deje volver hacia atras en la screen de parkin
-*   que deje volver hacia atras en la screen de restauracion sin el boton de vuelta
-* Hecho -> que deje hacer screendown en la parte de restaurantes
-* HECHO -> foto del parking ajustado al tamaño de pantalla del phone
- */
